88f79464cb15fbf80b4c7becf10b9b454a287d4f
ArchibaldLeMagnifique <denis.lachartre@free.fr>
2017-10-17 23:46:04 +0200

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-/*
-    ----- INITIALISATION -----
- */
 function readSingleFile(e) {
     var file = e.target.files[0];
     if (!file) {
@@ -11,7 +8,7 @@ function readSingleFile(e) {
         var contents = e.target.result;
         var splitByLine = contents.split("\n");
 
-        var X = [], Y = [];
+        var X=[], Y=[];
 
         for (var i = 0; i<splitByLine.length; i++) {
 
@@ -24,12 +21,26 @@ function readSingleFile(e) {
             X.push(parseFloat(splitByWordMinusSpaces[0]));
             Y.push(parseFloat(splitByWordMinusSpaces[1]));
         }
-        displayImage("firstCanvas", X, Y);
-
-        decompositionTotale(X, Y);
-
+        traitementDonnees(X, Y);
     };
     reader.readAsText(file);
+}
+
+function traitementDonnees(X, Y) {
+    displayImage("firstCanvas", X, Y);
+    var seuil = parseFloat(document.getElementById("seuil").value);
+    [tmpX, tmpY] = decompositionTotale(X, Y, seuil, 0, 0);
+    [resX, resY] = recompositionTotale(tmpX, tmpY);
+    displayImage("secondCanvas", resX, resY);
+
+    calculErreurs(X, Y);
+
+    var freq = parseFloat(document.getElementById("freq").value);
+    var effet = parseFloat(document.getElementById("effet").value);
+    displayImage("firstCanvas2", X, Y);
+    [tmpX, tmpY] = decompositionTotale(X, Y, 0, freq, effet);
+    [resX, resY] = recompositionTotale(tmpX, tmpY);
+    displayImage("secondCanvas2", resX, resY);
 }
 
 function refresh(){
@@ -39,14 +50,47 @@ function refresh(){
 document.getElementById('file-input')
     .addEventListener('change', readSingleFile, false);
 
+window.onload = function () {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        data: [ {type: "spline", dataPoints: []}]
+    });
+    chart.render();
+};
+
+/*
+    ----- ERREURS -----
+ */
+function calculErreurs(X, Y) {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        data: [ {type: "spline", dataPoints: []}]
+    });
+    chart.render();
+
+    for (var seuil=0; seuil<3; seuil+=0.1) {
+        [tmpX, tmpY] = decompositionTotale(X, Y, seuil, 0, 0);
+        [resX, resY] = recompositionTotale(tmpX, tmpY);
+        var a = differenceMoyenne(X, Y, resX, resY);
+        if (a<0.00001) a=0;
+        chart.data[0].addTo("dataPoints", {x:seuil, y:a});
+    }
+}
+
+function differenceMoyenne(X, Y, resX, resY) {
+    var res = 0;
+    for (var i = 0; i<X.length; i++) {
+        res+=Math.sqrt(Math.pow(resX[i]-X[i],2) + Math.pow(resY[i]-Y[i],2));
+    }
+    res/=X.length;
+    return res;
+}
+
 
 /*
     ----- DECOMPOSITION -----
  */
-function etapeDecomposition(X, Y, etape) {
+function etapeDecomposition(X, Y, etape, seuil, freq, effet) {
     var resX = X.slice(), resY = Y.slice();
     var t = X.length/Math.pow(2, etape);
-    var seuil = parseFloat(document.getElementById("seuil").value);
 
     for (var i = 0; i < t; i++) {
         resX[i] = 1/4 * (-X[mod(2*i-2, 2*t)] + 3*X[mod(2*i-1, 2*t)] + 3*X[mod(2*i, 2*t)] - X[mod(2*i+1, 2*t)]);
@@ -54,6 +98,12 @@ function etapeDecomposition(X, Y, etape) {
 
         resX[i+t] = 1/4 * (X[mod(2*i-2, 2*t)] - 3*X[mod(2*i-1, 2*t)] + 3*X[mod(2*i, 2*t)] - X[mod(2*i+1, 2*t)]);
         resY[i+t] = 1/4 * (Y[mod(2*i-2, 2*t)] - 3*Y[mod(2*i-1, 2*t)] + 3*Y[mod(2*i, 2*t)] - Y[mod(2*i+1, 2*t)]);
+
+        if (Math.random()*100<freq) {
+            resX[i] += (Math.random()-0.5) * effet;
+            resY[i] += (Math.random()-0.5) * effet;
+        }
+
         if (Math.sqrt(Math.pow(resX[i+t], 2) + Math.pow(resY[i+t], 2)) < seuil) {
             resX[i+t] = 0;
             resY[i+t] = 0;
@@ -62,12 +112,12 @@ function etapeDecomposition(X, Y, etape) {
     return [resX, resY];
 }
 
-function decompositionTotale(X, Y) {
+function decompositionTotale(X, Y, seuil, freq, effet) {
     var resX = X.slice(), resY = Y.slice();
     for (var i=1; i<=Math.log(X.length)/Math.log(2); i++) {
-        [resX, resY] = etapeDecomposition(resX, resY, i);
+        [resX, resY] = etapeDecomposition(resX, resY, i, seuil, freq, effet);
     }
-    recompositionTotale(resX, resY);
+    return [resX, resY];
 }
 
 /*
@@ -91,7 +141,7 @@ function recompositionTotale(X, Y) {
     for (var i=0; i<Math.log(X.length)/Math.log(2); i++) {
         [resX, resY] = etapeRecomposition(resX, resY, i);
     }
-    displayImage("secondCanvas", resX, resY);
+    return [resX, resY];
 }
 
 /*
